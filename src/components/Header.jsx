8965c6b26bd0815e1217f564d3765cf7f8465b46
Adriano Romero <adriano.romerofco@gmail.com>
2023-04-11 16:24:19 -0300

    feat: added home bio

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Sun } from "./svg/Sun.jsx";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h1></h1>
+        <div>
+          <Sun />
+        </div>
+      </nav>
+    </header>
+  );
+}
